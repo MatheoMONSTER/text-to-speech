d44209f5185a225f93f8729c6badbb162996368e
MatheoMONSTER <81566430+MatheoMONSTER@users.noreply.github.com>
2022-10-22 13:32:52 +0200

    Add files via upload

--- tts.py
+++ tts.py
@@ -2,10 +2,14 @@ from gtts import gTTS
 import os
 
 def textToSpeech(): 
-    someText = input("Enter some text: ")
-    audio = gTTS(text = someText, lang = "pl", slow = False)
+    try: 
+        someText = input("Enter some text: ")
+        language = input("Enter your language code: ")
+        audio = gTTS(text = someText, lang = language, slow = False)
 
-    audio.save("example.mp3") 
-    os.system("start example.mp3")
+        audio.save("example.mp3") 
+        os.system("start example.mp3")
+    except: 
+        print("An error occured")
 
 textToSpeech() 
